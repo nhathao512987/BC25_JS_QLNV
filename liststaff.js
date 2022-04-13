a22fcffb97f5b2eba28644e75864bbc45f405685
nhathao512987 <nguyenhanhathao@gmail.com>
2022-04-13 21:50:42 +0700

    nop bai

---
+++ liststaff.js
@@ -0,0 +1,8 @@
+function listStaff () {
+    this.arr = [];
+
+    this.addStaff= function () {};
+    this.removeStaff= function () {};
+    this.fixStaff= function () {};
+    this.updateStaff=function () {};
+}
---
+++ staff.js
@@ -0,0 +1,19 @@
+function Staff (
+    _ID,
+    _Name,
+    _Email,
+    _Password,
+    _Salary,
+    _Position,
+    _Day) {
+    
+    /* Khai báo thuộc Tính */
+    this.ID =_ID;
+    this.Name=_Name;
+    this.Email=_Email;
+    this.Password=_Password;
+    this.Salary=_Salary;
+    this.Positon=_Position;
+    this.Day=_Day;
+    
+}
